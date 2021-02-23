72f1cb5b48f972d95cc10a392d7dcd357e0aaa03
Jiahui <38282832+NonoHh@users.noreply.github.com>
2021-02-23 20:01:18 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -33,11 +33,11 @@ Scripts2URL=https://github.com/shylocks/Loon
 
 if [[ ${WhichDep} == *github* ]]; then
 #   ScriptsURL=https://github.com/LXK9301/jd_scripts
-  ScriptsURL=https://gitlab.com/MrRight/Scripts.git
+  ScriptsURL=https://gitlab.com/MrRight/Scripts
   ShellURL=https://github.com/NonoHh/jd-base
 else
 #   ScriptsURL=https://gitee.com/lxk0301/jd_scripts
-  ScriptsURL=https://gitlab.com/MrRight/Scripts.git
+  ScriptsURL=https://gitlab.com/MrRight/Scripts
   ShellURL=https://gitee.com/evine/jd-base
 fi
 
